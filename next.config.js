4b1659c67f2854e8d893c5141d84d4508747dfe0
bkalafat <kalafatburak@gmail.com>
2021-04-29 10:34:54 +0300

    refactor: Remove unused

--- next.config.js
+++ next.config.js
@@ -1,41 +1,9 @@
-const withOffline = require('next-offline')
+module.exports = {
 
-module.exports = withOffline({
-  workboxOpts: {
-    swDest: process.env.NEXT_EXPORT
-      ? 'service-worker.js'
-      : 'static/service-worker.js',
-    runtimeCaching: [
-      {
-        urlPattern: /^https?.*/,
-        handler: 'NetworkFirst',
-        options: {
-          cacheName: 'offlineCache',
-          expiration: {
-            maxEntries: 200,
-          },
-        },
-      },
-    ],
-  },
   images: {
     domains: ['firebasestorage.googleapis.com'],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('./scripts/generate-sitemap');
-    }
-    return config;
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/service-worker.js',
-        destination: '/_next/static/service-worker.js',
-      },
-    ]
   },
   future: {
     webpack5: true,
   },
-})
+}
