3e3129602e1d985073f896817cbd4c958c687f2f
bkalafat <kalafatburak@gmail.com>
2021-05-03 09:14:34 +0300

    feat: Add kunye.

---
+++ src/pages/kunye.tsx
@@ -0,0 +1,136 @@
+import Layout from "../components/Layout"
+import Head from "next/head"
+
+const PrivacyPolicy = () => {
+  return (<>
+
+    <Layout>
+      <Head><title>Künye</title></Head>
+      <div className="center-item">
+        <div className="hbrmItem">
+          <div className="hbrmTitle">
+            TSKulis.com
+          </div>
+          <div className="hbrmText">
+            <span>
+              Adres 	:	Çamlıca Mah. Hükümet Cad. No:26, Sürmene / Trabzon<br />
+              Telefon		: 	+90 462 339 09 98<br />
+              Faks		: 	+90 462 339 09 58<br />
+              Email		:	kulistrabzonspor@gmail.com
+            </span>
+          </div>
+        </div>
+
+        <div className="hbrmItem">
+          <div className="hbrmTitle">
+            <span>Sahibi</span>
+          </div>
+          <div className="hbrmText">
+            <span>Trabzon Spor Kulis Ltd Şti</span>
+          </div>
+        </div>
+        <div className="hbrmItem">
+          <div className="hbrmTitle">
+            <span>CEO</span>
+          </div>
+          <div className="hbrmText">
+            <span>Mir Çolakoğlu</span>
+          </div>
+        </div>
+
+        <div className="hbrmItem">
+          <div className="hbrmTitle">
+            <span>Yasal Bildirimler İçin</span>
+          </div>
+          <div className="hbrmText">
+            <span>
+              Tüm hukuki sorumluluk Teymur Hukuk üzerindedir.<br />
+              Hukuk Danışmanı 			: 	Av. Bedia Teymur<br />
+              <a href="mailto:iletisim@teymurhukuk.com" title="iletisim@teymurhukuk.com adresine mail gönderin">iletisim@teymurhukuk.com</a>
+            </span>
+          </div>
+        </div>
+
+
+        <div className="hbrmItem">
+          <div className="hbrmTitle">
+            <span>Haber Müdürü</span>
+          </div>
+          <div className="hbrmText">
+            <span>Ayça Kurtuluş Elal
+              <br /><a href="mailto:ayca.elal@tskulis.com" title="ayca.elal@tskulis.com adresine mail gönderin">ayca.elal@tskulis.com</a>
+            </span>
+          </div>
+        </div>
+
+        <div className="hbrmItem">
+          <div className="hbrmTitle">
+            <span>Haber Müdür Yardımcısı</span>
+          </div>
+          <div className="hbrmText">
+            <span>Olgun Kızıltepe
+              <br /><a href="mailto:olgun.kiziltepe@tskulis.com" title="olgun.kiziltepe@tskulis.com adresine mail gönderin">olgun.kiziltepe@tskulis.com</a>
+            </span>
+          </div>
+        </div>
+
+        <div className="hbrmItem">
+          <div className="hbrmTitle">
+            <span>Ekonomi Müdürü</span>
+          </div>
+          <div className="hbrmText">
+            <span>Mesut Şahin
+              <br /><a href="mailto:mesut.sahin@tskulis.com" title="mesut.sahin@tskulis.com adresine mail gönderin">mesut.sahin@tskulis.com</a>
+            </span>
+          </div>
+        </div>
+
+        <div className="hbrmItem">
+          <div className="hbrmTitle">
+            <span>Spor Müdürü</span>
+          </div>
+          <div className="hbrmText">
+            <span>Ömer Özarslan
+              <br /><a href="mailto:omer.ozarslan@tskulis.com" title="omer.ozarslan@tskulis.com adresine mail gönderin">omer.ozarslan@tskulis.com</a>
+            </span>
+          </div>
+        </div>
+
+        <div className="hbrmItem">
+          <div className="hbrmTitle">
+            <span>Magazin Müdürü</span>
+          </div>
+          <div className="hbrmText">
+            <span>Ecem Altan
+              <br /><a href="mailto:ecem.altan@tskulis.com" title="ecem.altan@tskulis.com adresine mail gönderin">ecem.altan@tskulis.com</a>
+            </span>
+          </div>
+        </div>
+
+
+        <div className="hbrmItem">
+          <div className="hbrmTitle">
+            <span>Reklam Direktörü</span>
+          </div>
+          <div className="hbrmText">
+            <span>Esra Şimşek
+              <br />+90 216 339 09 98
+              <br /><a href="mailto:esra.simsek@tskulis.com" title="esra.simsek@tskulis.com adresine mail gönderin">esra.simsek@tskulis.com</a>
+            </span>
+          </div>
+        </div>
+
+
+
+        <div className="hbrmItem">
+          <div className="hbrmText">
+            <span>TSKulis.com web sitesinde yayınlanan haber, resim, bilgi, belge, metin, video niteliğindeki tüm yazılı ve görsel eserler Türkiye Cumhuriyeti Yasalarına tamamen uygun olarak yayınlanmaktadır. TC 5846 sayılı Fikir ve Sanat Eserleri Kanunu'nun telif haklarına ilişkin hükümlerine ve AB Fikri Mülkiyet Hukukuna göre TSKulis.com'un yazılı izni olmadıkça hiçbir kimse, yayıncı ve kuruluş, herhangi bir eserin tamamını veya bir kısmını yayınlayamaz, çoğaltamaz, alıntı yapamaz.</span>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  </>)
+}
+
+
+export default PrivacyPolicy
