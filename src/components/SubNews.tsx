7ad720f70f7398179a03f909d9b6f6d46bf6ae25
bkalafat <kalafatburak@gmail.com>
2021-06-10 17:15:04 +0300

    feat: try google ads

--- src/components/SubNews.tsx
+++ src/components/SubNews.tsx
@@ -2,13 +2,24 @@ import React from "react"
 import { NewsType } from "../types/NewsType"
 import SubNewsCard from "./cards/SubNewsCard"
 
-const SubNews = ({newsList}: {newsList: NewsType[]}) => {
+const SubNews = ({ newsList }: { newsList: NewsType[] }) => {
 
   return (
     <div className="subNews clickable">
       {newsList.map((news) => (
         SubNewsCard(news)
       ))}
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+
+      <ins className="adsbygoogle"
+        style={{display:"block"}}
+        data-ad-client="ca-pub-9881133041867885"
+        data-ad-slot="5239195021"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({ });
+      </script>
     </div>
   )
 }
