7af429b9ed93da06b6b79db169f23a51c40a37c8
bkalafat <kalafatburak@gmail.com>
2021-06-25 18:37:47 +0300

    rollback: Show ads if browser

--- src/components/SubNews.tsx
+++ src/components/SubNews.tsx
@@ -1,20 +1,8 @@
-import React, { FC, useEffect } from "react"
+import React, { FC } from "react"
 import { NewsType } from "../types/NewsType"
 import SubNewsCard from "./cards/SubNewsCard"
 
-declare global {
-  interface Window {
-    adsbygoogle: {[key: string]: unknown}[]
-  }
-}
-
 const SubNews : FC<{ newsList: NewsType[] }> = ({ newsList }: { newsList: NewsType[] }) => {
-
-  useEffect(()=> {
-    if(typeof window !== 'undefined'){
-          (window.adsbygoogle = window.adsbygoogle || []).push({})
-    }
-}, [])
 
   return (
     <div className="subNews clickable">
@@ -29,9 +17,11 @@ const SubNews : FC<{ newsList: NewsType[] }> = ({ newsList }: { newsList: NewsTy
         data-ad-slot="5239195021"
         data-ad-format="auto"
         data-full-width-responsive="true"></ins>
+      <script>
+        (adsbygoogle = window.adsbygoogle || []).push({ });
+      </script>
     </div>
   )
 }
 
 export default SubNews
-
