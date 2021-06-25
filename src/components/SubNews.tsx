d9f6d53841045ebe7c9128972a96e9c72e392cb3
bkalafat <kalafatburak@gmail.com>
2021-06-25 18:33:13 +0300

    feat: Show ads if env browser

--- src/components/SubNews.tsx
+++ src/components/SubNews.tsx
@@ -1,8 +1,20 @@
-import React, { FC } from "react"
+import React, { FC, useEffect } from "react"
 import { NewsType } from "../types/NewsType"
 import SubNewsCard from "./cards/SubNewsCard"
 
+declare global {
+  interface Window {
+    adsbygoogle: {[key: string]: unknown}[]
+  }
+}
+
 const SubNews : FC<{ newsList: NewsType[] }> = ({ newsList }: { newsList: NewsType[] }) => {
+
+  useEffect(()=> {
+    if(typeof window !== 'undefined'){
+          (window.adsbygoogle = window.adsbygoogle || []).push({})
+    }
+}, [])
 
   return (
     <div className="subNews clickable">
@@ -17,9 +29,6 @@ const SubNews : FC<{ newsList: NewsType[] }> = ({ newsList }: { newsList: NewsTy
         data-ad-slot="5239195021"
         data-ad-format="auto"
         data-full-width-responsive="true"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({ });
-      </script>
     </div>
   )
 }
