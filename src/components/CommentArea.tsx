bda76998f14a4fb7554a05661e66518f28c1448c
bkalafat <kalafatburak@gmail.com>
2021-07-01 13:41:34 +0300

    feat: Use bootstrap instead semanticUi for comment

--- src/components/CommentArea.tsx
+++ src/components/CommentArea.tsx
@@ -1,9 +1,6 @@
 import { useState } from 'react'
-import { Comment, Form, Header } from 'semantic-ui-react'
 import { CommentType } from '../types/CommentType'
 import * as API from "../utils/api"
-import PopUpButton from './PopUpButton'
-import 'semantic-ui-css/semantic.min.css'
 
 
 const CommentArea = ({ newsId, comments }: { newsId: string, comments: CommentType[] }) => {
@@ -26,31 +23,34 @@ const CommentArea = ({ newsId, comments }: { newsId: string, comments: CommentTy
   }
 
   return (
-    <Comment.Group className="maxWidth">
-      <Header as='h3' dividing>
-        Yorumlar
-      </Header>
-      {comments.map(c => {
-        let [y, m, d, hh, mm, ss, ms] = c.createDate.match(/\d+/g)
-        let date = new Date(Date.UTC(+y, +m - 1, +d, +hh, +mm, +ss, +ms))
-        let formatted = date.toLocaleString()
-        return <Comment key={c.id}>
-          <Comment.Content>
-            <Comment.Author as='a'>{c.userName}</Comment.Author>
-            <Comment.Metadata>
-              <div>{formatted}</div>
-            </Comment.Metadata>
-            <Comment.Text>{c.text}</Comment.Text>
-          </Comment.Content>
-        </Comment>
-      })}
-      <Form onSubmit={onSubmit} reply>
-        <Form.Input placeholder="Kullanıcı Adı (en az 4 karakter)" onChange={e => setComment({ ...comment, userName: e.target.value })} value={comment.userName} />
-        <Form.TextArea style={{ height: 60 }} placeholder="Yorum (en az 30 karakter)" onChange={e => setComment({ ...comment, text: e.target.value })} value={comment.text} />
-        {comment.text.length < minCommentLength ? <div>{minCommentLength - comment.text.length} karakter kaldı.</div> : comment.userName.length > 3 ? <div>Uygun</div> : <div>Kullanıcı Adı giriniz.</div>}
-        <PopUpButton buttonName="Yorum Yap" content='Onaya gönderildi.' header="Başarılı" active={comment.text.length >= minCommentLength && comment.userName.length > 3} />
-      </Form>
-    </Comment.Group>
+    <div className="container mt-5">
+      <div className="d-flex justify-content-center row">
+        <div className="col-md-8">
+          <div className="d-flex flex-column comment-section">
+            {comments.map(c => {
+              let [y, m, d, hh, mm, ss, ms] = c.createDate.match(/\d+/g)
+              let date = new Date(Date.UTC(+y, +m - 1, +d, +hh, +mm, +ss, +ms))
+              let formatted = date.toLocaleString()
+              return <> <div className="bg-white p-2">
+                <div className="d-flex flex-row user-info">
+                  <div className="d-flex flex-column justify-content-start ml-2"><span className="d-block font-weight-bold name">{c.userName}</span><span className="date text-black-50">{formatted}</span></div>
+                </div>
+                <div className="mt-2">
+                  <p className="comment-text">{c.text}</p>
+                </div>
+              </div></>
+            })}
+            <div className="bg-light p-2">
+              <div className="d-flex flex-row align-items-start"><input placeholder="Kullanıcı Adı (en az 4 karakter)" onChange={e => setComment({ ...comment, userName: e.target.value })} value={comment.userName} className="form-control ml-1 shadow-none textarea"></input></div>
+              <br />
+              <div className="d-flex flex-row align-items-start"><textarea placeholder="Yorum (en az 30 karakter)" onChange={e => setComment({ ...comment, text: e.target.value })} value={comment.text} className="form-control ml-1 shadow-none textarea"></textarea></div>
+              {comment.text.length < minCommentLength ? <div>{minCommentLength - comment.text.length} karakter kaldı.</div> : comment.userName.length > 3 ? <div>Uygun</div> : <div>Kullanıcı Adı giriniz.</div>}
+              <div className="mt-2 text-right"><button onSubmit={onSubmit} disabled={comment.text.length <= minCommentLength || comment.userName.length < 4} className="btn btn-primary btn-sm shadow-none" type="button">Yorum yap</button></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
