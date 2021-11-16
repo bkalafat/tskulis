c5d9afc5a9700d4cbf50873dd247defd8037542f
bkalafat <kalafatburak@gmail.com>
2021-11-16 22:37:27 +0300

    REMOVE NEXT PWA

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
-const withPWA = require('next-pwa')
-
-module.exports = withPWA({
+module.exports = {
   images: {
     domains: ['firebasestorage.googleapis.com'],
-  },
-  pwa: {
-    dest: 'public'
   }
-})
+}
