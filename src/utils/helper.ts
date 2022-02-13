da9cb6091969436cb6c2586c21c47af86db9dfe8
bkalafat <kalafatburak@gmail.com>
2022-02-13 14:39:23 +0300

    feat: Add berker

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -75,7 +75,7 @@ export const sortCreateDateDesc = () => {
 }
 
 export const getAdmins = () => {
-  return ["Burak KALAFAT", "fatalarr@gmail.com", "ktuna17@outlook.com", "Mustafa Mir Çolakoğlu 🇹🇷", "burakkalafat@hotmail.com", "barisatmaca061@gmail.com", "aberker61@gmail.com", "berkerkucuk1@gmail.com", "zaferhalatci@gmail.com"];
+  return ["Burak KALAFAT", "fatalarr@gmail.com", "ktuna17@outlook.com", "Mustafa Mir Çolakoğlu 🇹🇷", "burakkalafat@hotmail.com", "barisatmaca061@gmail.com", "Berker Küçük", "berkerkucuk1@gmail.com", "zaferhalatci@gmail.com"];
 }
 
 export const ShowMedias = (content: string) => {
