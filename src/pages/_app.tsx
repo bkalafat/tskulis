ec540f631b1166ec60450dbdac90155f058d91ca
Burak.Kalafat <Burak.Kalafat@insightsoftware.com>
2022-12-05 15:37:24 +0300

    feat: Update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 import '../content-styles.css'
+import { Session } from "next-auth";
 
 
 import { SessionProvider } from "next-auth/react"
@@ -10,7 +11,7 @@ import "reflect-metadata";
 import type { AppProps } from 'next/app'
 
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps<{ session: Session;}>) {
   return <SessionProvider session={pageProps.session} refetchInterval={5 * 60} >
     <Component {...pageProps} />
   </SessionProvider>
